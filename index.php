b4f1380c371ecf84f1ed2f82c84cd09f4587e2b3
151250063 <151250063@smail.nju.edu.cn>
2017-05-16 01:00:38 +0900

    添加数据库连接，网页访问者计数

--- index.php
+++ index.php
@@ -12,9 +12,43 @@ $country=$ar["country"];
 $region=$ar["region"];
 $city=$ar["city"];
 $isp=$ar["isp"];
+$datetime = new \DateTime;
+$time=$datetime->format('Y-m-d H:i:s');
+
+//读取
+$mysql_server_name='138.68.41.21';
+$mysql_username='root';
+$mysql_password='1248163264128';
+$mysql_database='visited_ip';
+$conn=mysql_connect($mysql_server_name, $mysql_username, $mysql_password) or die("error connecting") ;
+mysql_query("set names 'utf8'");
+mysql_select_db($mysql_database);
+
+//查找此ip是否访问
+$sqlexist = "select distinct(ip_id) from ip_info where ip_address='$yourip'";
+$ipexist = mysql_query($sqlexist);
+if (!mysql_num_rows($ipexist)) {
+    $sqlnum = "select distinct(ip_address) from ip_info";
+    $ipdb = mysql_query($sqlnum);
+    $ipnum = mysql_num_rows($ipdb);
+    $ipid = $ipnum+1;
+    echo "欢迎第一次访问过本站。你是本站第".$ipid."位访问者。</br>";
+    //写入数据库
+    $sqlwrite = "insert into ip_info (ip_id, ip_address,ip_location,time) values ($ipid,'$yourip','$country$region$city$isp','$time')";
+    mysql_query($sqlwrite);
+    mysql_close();
+} else {
+    $ipid = mysql_fetch_array($ipexist)['ip_id'];
+    echo "你曾经访问过本站。你曾经是本站第".$ipid."位访问者。</br>";
+    //写入数据库
+    $sqlwrite = "insert into ip_info (ip_id, ip_address,ip_location,time) values ($ipid,'$yourip','$country$region$city$isp','$time')";
+    mysql_query($sqlwrite);
+    mysql_close();
+}
 
 echo "你的ip是".$yourip."</br>";
-echo "来自".$country.$region.$city." ".$isp;
+echo "来自".$country.$region.$city." ".$isp."</br>";
+echo "现在时间 ".$time;
 
 function getip()//获取ip
 {
