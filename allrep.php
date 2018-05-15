cc65d790e37623a425910a6b0b06dbb0a10117c0
151250063 <151250063@smail.nju.edu.cn>
2018-05-15 22:35:47 +0900

    文件排序

--- allrep.php
+++ allrep.php
@@ -7,12 +7,14 @@ function my_dir($dir) {
                 if(is_dir($dir."/".$file)) { //如果是子文件夹，就进行递归
                     $files[$file] = my_dir($dir."/".$file);
                 } else { //不然就将文件的名字存入数组；
+                    $filetime [] = date("Y-m-d H:i:s", filemtime($dir."/".$file));
                     $files[] = $file;
                 }
 
             }
         }
         closedir($handle);
+        array_multisort($filetime,SORT_DESC,SORT_STRING, $files);
         return $files;
     }
 }
@@ -20,8 +22,10 @@ header("content-type:text/html;charset=utf-8");
 $pass=$_GET['pass'];
 if($pass=="b347342e7b6295d4b5c96889c3d0dd23"){
     $rpys=my_dir("./php/rep");
+    $num=count($rpys);
     foreach ($rpys as $rpy) {
-        echo "<a href='http://www.cirno.com.cn/represult.php?rep=".$rpy."'>".$rpy."</a></br>";
+        echo $num.".&nbsp;&nbsp;&nbsp;<a href='http://www.cirno.com.cn/represult.php?rep=".$rpy."'>".$rpy."</a></br>";
+        $num-=1;
     }
 }else{
     echo "你没有权限，请和网站建立者PY以获得权限。";
